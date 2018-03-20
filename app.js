731a7ad92051f5af0d7e7fc7c5f045509edc831c
Moser <noralmoser@gmail.com>
2018-03-20 14:04:23 -0500

    calling to an api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const request = require('request');
+
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyDwvJ2tcM0qGJP-Fc0W9_ce5Eoj7MTsndw',
+    json: true
+}, (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
