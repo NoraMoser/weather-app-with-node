4bdedf75fbe2af350ef9e7b9a8f47ca73dda8436
Moser <noralmoser@gmail.com>
2018-03-21 13:04:33 -0500

    added callbacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -15,20 +16,13 @@ const argv = yargs
     .argv;
 
 // console.log(argv);
-
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDwvJ2tcM0qGJP-Fc0W9_ce5Eoj7MTsndw`,
-    json: true
-}, (error, response, body) => {
-    if(error) {
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('No address matched that query.')
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log("there was an error");
+    } else if (results) {
+        console.log(JSON.stringify(results, undefined, 5));
     }
+
 });
+
+
