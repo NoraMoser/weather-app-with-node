c501492847069ff7abbf16fd7351e930376e4bf0
Moser <noralmoser@gmail.com>
2018-03-22 11:30:13 -0500

    do the green dots work?

--- weather/weather.js
+++ weather/weather.js
@@ -9,8 +9,8 @@ weatherInformation = () => {
     }, (error, response, body) => {
         if (error) {
             console.log('Unable to connect to Weather servers.');
-        } else if (response.statusCode === 400) {
-            console.log =('Unable to fetch weather')
+        // } else if (response.statusCode === 400) {
+        //     console.log =('Unable to fetch weather')
         } else if (response.statusCode === 200) {
             console.log('temperature:', body.currently.temperature);
         }
